0c9246f68a37a5d7477667d1ef7d027d656ebb06
karsh <utkarsh@cyberfear.com>
2024-04-10 10:57:39 +1000

    easier build and run
    + added build alias to compile with bootimage
    + added run alias to start qemu session with bootimage

--- serenity.session.sh
+++ serenity.session.sh
@@ -5,12 +5,15 @@ if initialize_session "serenity"; then
   run_cmd "v -O2 ./src/main.rs ./src/vga_buffer.rs"
 
   new_window "shell"
+  run_cmd "nix-shell --run zsh"
+  run_cmd "alias build='cargo bootimage'"
+  run_cmd "alias run='qemu-system-x86_64 -drive format=raw,file=target/x86_64-serinity_os/debug/bootimage-serenity_os.bin'"
+  run_cmd "clear"
+
   split_h
   run_cmd "giti"
+  run_cmd "c todo"
   select_pane 1
-
-  select_window 1
-
 fi
 
 finalize_and_go_to_session
