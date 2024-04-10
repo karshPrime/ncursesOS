bf8e8ceabc2195d7b26041ebe9f053f91a72ba03
karsh <utkarsh@cyberfear.com>
2024-04-10 10:59:50 +1000

    update shell.nix
    ~ fix indentation

--- shell.nix
+++ shell.nix
@@ -3,17 +3,17 @@
 with (import <nixpkgs> {});
 mkShell {
     buildInputs = [
-	pkgs.neovim
+        pkgs.neovim
         pkgs.rustup
         pkgs.cargo
         pkgs.qemu
     ];
 
-    # Install dependencies
+# Install dependencies
     shellHook = ''
-	rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
-	rustup component add llvm-tools-preview
-	cargo install bootimage
+        rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+        rustup component add llvm-tools-preview
+        cargo install bootimage
     '';
 }
 
